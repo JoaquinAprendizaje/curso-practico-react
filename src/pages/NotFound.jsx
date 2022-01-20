497666aa74ae52953d425ec2da6aba7416b76da7
joaquinaprendizaje <joaquinarturoaprendizaje@gmail.com>
2022-01-20 13:24:18 -0800

    Got the solution of challenge with components and routes.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import '../styles/NotFound.scss'
+
 const NotFound = () => {
-    return (
-        <div>
-            <h1 className="title-404">ERROR 404</h1>
-            <p className="text-404">Oops! Page Not Found</p>
-            <a className="button-404" href="#">Return to Main Page</a>
-        </div>
-    );
+	return (
+		<div>
+			No encontrado, error 404.
+		</div>
+	);
 }
 
 export default NotFound;
